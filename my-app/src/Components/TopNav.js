ed158ef33d8a447e1beb121bb8f1b6a39c13819f
James Ho <8.james.ho@gmail.com>
2022-07-11 00:45:08 -0400

    tried adding dynamic scaling

--- my-app/src/Components/TopNav.js
+++ my-app/src/Components/TopNav.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import links from "../Navlinks/Navlinks.json";
 import { Link, useLocation } from "react-router-dom";
 
 export default function TopNav() {
+  const [mouseX, setMouseX] = useState(0);
+  const [mouseY, setMouseY] = useState(0);
+  const [dist, setDist] = useState(0);
+  const [scale, setScale] = useState(0);
+  const myRef = useRef();
+
+  function getCenterOfElement() {
+    const h = myRef.current.offsetHeight;
+    const w = myRef.current.offsetWidth;
+    // Calculate centers of the elements
+    const X = myRef.current.offsetLeft + 0.5 * w;
+    const Y = myRef.current.offsetTop + 8 * h;
+    console.log(X, Y, h, w);
+    return { X, Y };
+  }
+
+  function onMouseMove(e) {
+    let x = e.screenX;
+    let y = e.screenY;
+    let { X, Y } = getCenterOfElement();
+    setMouseX(x);
+    setMouseY(y);
+    setDist(getDistance(x, y, X, Y));
+    setScale((5 / dist) * 1.5);
+  }
+
+  function getDistance(x, y, X, Y) {
+    return Math.sqrt(Math.abs(x - X) ** 2 + Math.abs(y - Y) ** 2);
+  }
   // This shifts the UI so the selected item is always first
   const location = useLocation(); // get the current path of the webpage
   let pageName = location.pathname.slice(1).toLocaleLowerCase(); // gets the current page name
@@ -16,7 +45,10 @@ export default function TopNav() {
   //   lowercaseNavLinks.unshift(links.navLinks[removeId]);
 
   return (
-    <div className="flex flex-row text-sm gap-8 items-baseline">
+    <div
+      onMouseMove={onMouseMove}
+      className="flex flex-row text-sm gap-8 items-baseline"
+    >
       {lowercaseNavLinks.map((obj) => {
         let path = `/${obj.toLowerCase()}`;
         let currentNavItem = obj.toLocaleLowerCase();
@@ -30,13 +62,22 @@ export default function TopNav() {
           );
         }
         return (
-          <nav>
-            <Link to={path} className=" hover:underline capitalize">
-              {String(obj)}
-            </Link>
-          </nav>
+          <div>
+            <nav>
+              <Link
+                ref={myRef}
+                to={path}
+                className=" hover:underline capitalize"
+              >
+                {String(obj)}
+              </Link>
+            </nav>
+          </div>
         );
       })}
+      {/* <p>
+        {mouseX}, {mouseY}, {dist}, {scale}
+      </p> */}
     </div>
   );
 }
