3db85281622372f211449eb5b4c04612b9a51029
James Ho <8.james.ho@gmail.com>
2023-03-24 22:22:44 -0400

    added pfp to home page

--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -2,37 +2,48 @@ import React from "react";
 import { Link } from "react-router-dom";
 import links from "../Navlinks/Navlinks.json";
 import TypeAnimation from "react-type-animation";
+import pfp from "../Media/landing_page_pfp.png";
 
 export default function Home() {
   return (
-    <div className="h-full min-h-screen bg-white flex flex-col items-center">
-      <div className="w-4/5 md:w-2/3 lg:w-3/5 mt-40 flex flex-col gap-12 xl:w-auto xl:h-auto 2xl:w-auto">
-        <div className="text-5xl md:text-7xl lg:text-7xl font-bold">
-          <TypeAnimation
-            cursor={true}
-            sequence={["JAMES HO"]}
-            wrapper="a"
-            repeat={1}
-          />
-        </div>
-        <div className=" italic text-slate-500">
-          <p>Breadth in experiences and interests</p>
-          <p>Depth in mechanical engineering</p>
+    <div className="h-full min-h-screen bg-white flex flex-col items-center flex">
+      <div className="flex md:flex-row flex-col w-screen">
+        <div className="">
+          <img
+            className="md:object-cover md:h-screen md:w-auto w-screen h-[500px] object-cover object-top"
+            alt="Profile"
+            src={pfp}
+          ></img>
         </div>
 
-        <div className="flex flex-col text gap-2">
-          {links.navLinks.map((obj) => {
-            let path = `/${obj.toLowerCase()}`;
-            return (
-              <nav className="transition hover:scale-150 hover:font-bold w-0">
-                <Link to={path}>{String(obj)}</Link>
-              </nav>
-            );
-          })}
-        </div>
+        <div className="w-auto md:mt-40 lg:mt-40 flex flex-col gap-12 md:mx-8 lg:mx-auto m-16">
+          <div className="text-5xl md:text-7xl lg:text-7xl font-bold">
+            <TypeAnimation
+              cursor={true}
+              sequence={["JAMES HO"]}
+              wrapper="a"
+              repeat={1}
+            />
+          </div>
+          <div className=" italic text-slate-500">
+            <p>Breadth in experiences and interests</p>
+            <p>Depth in mechanical engineering</p>
+          </div>
 
-        <div className=" text-sm text-slate-500">
-          <p className=" underline">8.james.ho@gmail.com</p>
+          <div className="flex flex-col text gap-2">
+            {links.navLinks.map((obj) => {
+              let path = `/${obj.toLowerCase()}`;
+              return (
+                <nav className="transition hover:scale-150 hover:font-bold w-0">
+                  <Link to={path}>{String(obj)}</Link>
+                </nav>
+              );
+            })}
+          </div>
+
+          <div className=" text-sm text-slate-500">
+            <p className=" underline">8.james.ho@gmail.com</p>
+          </div>
         </div>
       </div>
     </div>
