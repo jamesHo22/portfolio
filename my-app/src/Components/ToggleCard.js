56884e0dba93210e179db90da1b3d4fe265b229e
jamesho22 <8.james.ho@gmail.com>
2024-07-21 15:24:15 -0400

    added reading list

--- my-app/src/Components/ToggleCard.js
+++ my-app/src/Components/ToggleCard.js
@@ -3,30 +3,24 @@ import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import filler from "../Filler/filler.json";
 
-export default function ItemCard(props) {
+export default function ToggleCard(props) {
   let navigate = useNavigate();
   const location = useLocation();
-  let externalLink = props.content.full_article?.link;
-  console.log(externalLink);
+  const [toggleBody, setToggleBody] = useState(false);
 
   return (
     <div
-      className={
-        "bg-white rounded-lg transition h-fit lg:h-48 flex flex-col items-center lg:flex-row cursor-pointer hover:shadow-lg hover:shadow-slate-500/50"
-      }
+      className="w-full bg-white rounded-lg transition hover:shadow-lg hover:shadow-slate-500/50 h-fit lg:h-fit flex flex-col lg:flex-row cursor-pointer"
       onClick={() => {
-        if (externalLink !== undefined) {
-          window.location.href = externalLink;
-        }
+        setToggleBody(!toggleBody);
+        console.log(props.mdContent);
       }}
     >
-      <img
-        className="object-cover w-auto h-auto lg:w-48 lg:h-48 rounded-md lg:rounded-lg"
-        src={props.content.full_article.image_src}
-        alt="Logo"
-      />
-      <div className="mx-4 lg:mx-8 my-4 flex flex-col text-ellipsis overflow-hidden gap-4">
-        <h1 className="text-2xl xl:text-3xl font-bold">
+      <div className="mx-4 lg:mx-8 my-4 flex flex-col text-ellipsis overflow-hidden gap-2 ">
+        <div className="text-xs italic text-gray-600">
+          {props.content.full_article.author}
+        </div>
+        <h1 className="text-l xl:text-2xl">
           {props.content.full_article.title}
         </h1>
         {/* Check if the tags field exists */}
@@ -46,8 +40,13 @@ export default function ItemCard(props) {
           <div />
         )}
 
-        {/* <ReactMarkdown>{props.content.full_article.body}</ReactMarkdown> */}
-        <div className=" text-sm">{props.content.full_article.body}</div>
+        {toggleBody ? (
+          <div className="prose text-sm max-w-none my-4">
+            <ReactMarkdown>{props.content.full_article.body}</ReactMarkdown>
+          </div>
+        ) : (
+          <div></div>
+        )}
       </div>
     </div>
   );
--- my-app/src/Pages/Reading.js
+++ my-app/src/Pages/Reading.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TopNav from "../Components/TopNav";
 import ItemCard from "../Components/ItemCard";
 import filler from "../Filler/filler.json";
 import FunCard from "../Components/FunCard";
+import ToggleCard from "../Components/ToggleCard";
+import about from "../Content/Projects/About.md";
 
 export default function Reading() {
+  let [mdContent, setMdContent] = useState("");
+
+  useEffect(() => {
+    fetch(about)
+      .then((res) => {
+        return res.text();
+      })
+      .then((text) => {
+        console.log(text);
+        setMdContent(text);
+      });
+  }, []);
+
   return (
     <div className="h-full min-h-screen bg-white flex flex-col items-center">
       <div className="w-4/5 lg:w-2/3 my-20">
         <TopNav />
       </div>
 
-      <div className="w-4/5 lg:w-2/3 xl:w-2/3 2xl:w-[60rem] mb-20 flex flex-col gap-20">
-        <div className="flex flex-col gap-10 items-center">
+      <div className="w-4/5 lg:w-2/3 xl:w-2/3 2xl:w-[60rem] mb-20 flex flex-col gap-8">
+        <div className="text-2xl xl:text-3xl font-bold">Recent Reads</div>
+        <div className="flex flex-col gap-2 items-start">
           {filler.reading.map((obj) => {
             console.log(obj);
-            return <ItemCard content={obj} />;
+            return <ToggleCard content={obj} />;
           })}
         </div>
       </div>
